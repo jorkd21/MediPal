b5f32f497970ad5d292123e061c7be33cf229df1
kosmckid <kosmckid@gmail.com>
2024-03-10 22:01:22 -0700

    firebase integration testing implemented

--- gui_flutter/lib/main.dart
+++ gui_flutter/lib/main.dart
@@ -1,7 +1,15 @@
 import 'package:flutter/material.dart';
+// firebase
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  // run app
   runApp(const MyApp());
+  // initialize firebase (testing)
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
 }
 
 class MyApp extends StatelessWidget {
