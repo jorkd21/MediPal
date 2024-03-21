eaf594f848913a6ec0d507c7aeed103c98753a77
kosmckid <kosmckid@gmail.com>
2024-03-20 20:16:53 -0700

    added missing dependencies for dev env script

--- dev_env_setup.ps1
+++ dev_env_setup.ps1
@@ -45,6 +45,10 @@ sdkmanager --install "platform-tools"
 sdkmanager --install "system-images;android-34;google_apis;x86_64"
 sdkmanager --install "extras;intel;Hardware_Accelerated_Execution_Manager"
 sdkmanager --install "sources;android-34"
+sdkmanager --install "cmdline-tools;latest"
+
+# accept android licenses
+flutter doctor --android-licenses
 
 firebase login
 #flutterfire configure
