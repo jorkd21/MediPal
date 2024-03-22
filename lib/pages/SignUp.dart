b4c192f0b968e353e6f611a756063a4a12b1d8d9
Solbjor <s0lbjorandr3w@gmail.com>
2024-03-21 23:29:59 -0700

    Sign Up ui
    added logo

--- lib/pages/SignUp.dart
+++ lib/pages/SignUp.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:medipal/constant/images.dart';
 
 class SignUpPage extends StatefulWidget {
   @override
@@ -8,18 +9,40 @@ class SignUpPage extends StatefulWidget {
 class _SignUpPageState extends State<SignUpPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(),
-      body: 
-      Container(
-        decoration: BoxDecoration(
-          gradient: LinearGradient(
-            begin: Alignment.bottomCenter,
-            end: Alignment.topCenter,
-            colors: [
-              Color(0xFF6D98EB), // Light blue at the bottom
-              Color(0xFFBAA2DA), // Purple at the top
-            ],
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(),
+        body: 
+        Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.bottomCenter,
+              end: Alignment.topCenter,
+              colors: [
+                Color(0xFF6D98EB), // Light blue at the bottom
+                Color(0xFFBAA2DA), // Purple at the top
+              ],
+            ),
+          ),
+          child: Padding(
+            padding: EdgeInsets.all(20.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    Container(
+                      height: 100,
+                      width: 100,
+                      child: Image.asset(
+                        myImage,
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
           ),
         ),
       ),
