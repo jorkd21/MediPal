9af229f10747fa4ddac45ed88cf9a3657a7d8567
Solbjor <s0lbjorandr3w@gmail.com>
2024-03-26 00:32:38 -0700

    Added Shoe's Sign Up
    Added Shoe's Sign up ui and tweaked it

--- lib/pages/SignUp.dart
+++ lib/pages/SignUp.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:medipal/constant/images.dart';
-import 'package:medipal/pages/login.dart';
 
 class SignUpPage extends StatefulWidget {
   @override
@@ -8,13 +7,27 @@ class SignUpPage extends StatefulWidget {
 }
 
 class _SignUpPageState extends State<SignUpPage> {
+  final TextEditingController _firstNameController = TextEditingController();
+  final TextEditingController _lastNameController = TextEditingController();
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+  final TextEditingController _phoneNumberController = TextEditingController();
+  final TextEditingController _insuranceNumberController =
+      TextEditingController();
+  final TextEditingController _countryController = TextEditingController();
+
+  String _errorMessage = '';
+
+  void _signUp() async {
+
+  }
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        appBar: AppBar(),
-        body: 
-        Container(
+        appBar: AppBar(title: Text("Sign Up Page")),
+        body: Container(
           decoration: BoxDecoration(
             gradient: LinearGradient(
               begin: Alignment.bottomCenter,
@@ -25,28 +38,199 @@ class _SignUpPageState extends State<SignUpPage> {
               ],
             ),
           ),
+          
           child: Padding(
             padding: EdgeInsets.all(20.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.end,
+            child: SingleChildScrollView(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
                   children: [
-                    Container(
-                      height: 100,
-                      width: 100,
-                      child: Image.asset(
-                        myImage,
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      Container(
+                        height: 100,
+                        width: 100,
+                        child: Image.asset(
+                          myImage,
+                        ),
                       ),
+                    ],
+                    ),
+                    SizedBox(height: 17.0),
+                    SizedBox(height: 17.0),
+                    Padding(
+                      padding: const EdgeInsets.fromLTRB(0.0, 0.0, 100, 25.0),
+                      child: Text(
+                        'Create Account',
+                        style: TextStyle(
+                          fontSize: 35,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
+                        ),
+                        textAlign: TextAlign.left,
+                      ),
+                    ),
+                    SizedBox(height: 20.0),
+                    FractionallySizedBox(
+                      widthFactor: 0.75, // Set width factor to half
+                      child: Container(
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+      borderRadius: BorderRadius.circular(10.0),
+                        ),
+                        child: TextField(
+                          controller: _firstNameController,
+                          decoration: InputDecoration(
+                            labelText: 'First Name',
+                            border: InputBorder.none,
+                            contentPadding:
+                                EdgeInsets.symmetric(horizontal: 10.0),
+                          ),
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: 20.0),
+                    FractionallySizedBox(
+                      widthFactor: 0.75, // Set width factor to half
+                      child: Container(
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(10.0),
+                        ),
+                        child: TextField(
+                          controller: _lastNameController,
+                          decoration: InputDecoration(
+                            labelText: 'Last Name',
+                            border: InputBorder.none,
+                            contentPadding:
+                                EdgeInsets.symmetric(horizontal: 10.0),
+                          ),
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: 20.0),
+                    FractionallySizedBox(
+                      widthFactor: 0.75, // Set width factor to half
+                      child: Container(
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(10.0),
+                        ),
+                        child: TextField(
+                          controller: _emailController,
+                          decoration: InputDecoration(
+                            labelText: 'Email',
+                            border: InputBorder.none,
+                            contentPadding:
+                                EdgeInsets.symmetric(horizontal: 10.0),
+                          ),
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: 20.0),
+                    FractionallySizedBox(
+                      widthFactor: 0.75, // Set width factor to half
+                      child: Container(
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(10.0),
+                        ),
+                        child: TextField(
+                          controller: _phoneNumberController,
+                          decoration: InputDecoration(
+                            labelText: 'Password',
+                            border: InputBorder.none,
+                            contentPadding:
+                                EdgeInsets.symmetric(horizontal: 10.0),
+                          ),
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: 20.0),
+                    FractionallySizedBox(
+                      widthFactor: 0.75, // Set width factor to half
+                      child: Container(
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(10.0),
+                        ),
+                        child: TextField(
+                          controller: _insuranceNumberController,
+                          decoration: InputDecoration(
+                            labelText: 'Phone Number',
+                            border: InputBorder.none,
+                            contentPadding:
+                                EdgeInsets.symmetric(horizontal: 10.0),
+                          ),
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: 20.0),
+                    FractionallySizedBox(
+                      widthFactor: 0.75, // Set width factor to half
+                      child: Container(
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(10.0),
+                        ),
+                        child: TextField(
+                          controller: _countryController,
+                          decoration: InputDecoration(
+                            labelText: 'License Number',
+                            border: InputBorder.none,
+                            contentPadding:
+                                EdgeInsets.symmetric(horizontal: 10.0),
+                          ),
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: 20.0),
+                    FractionallySizedBox(
+                      widthFactor: 0.75, // Set width factor to half
+                      child: Container(
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(10.0),
+                        ),
+                        child: TextField(
+                          controller: _passwordController,
+                          decoration: InputDecoration(
+                            labelText: 'Country',
+                            border: InputBorder.none,
+                            contentPadding:
+                                EdgeInsets.symmetric(horizontal: 10.0),
+                          ),
+                          obscureText: true, // Hide the entered text
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: 40.0),
+                    Align(
+                      alignment: Alignment.center,
+                      child: ElevatedButton(
+                        onPressed: _signUp,
+                        style: ButtonStyle(
+                          minimumSize: MaterialStateProperty.all<Size>(Size(278.0, 44.0)),
+                          backgroundColor: MaterialStateProperty.all<Color>(Color(0xFF003CD6)),
+                        ),
+                        child: Text(
+                          'Create Account',
+                          style: TextStyle(color: Color(0xFFEFEFEF), fontSize: 20, fontStyle: FontStyle.normal),
+                        ),
+                      ),
+                    ),
+                    if (_errorMessage.isNotEmpty)
+                      Text(
+                        _errorMessage,
+                        style: TextStyle(color: Colors.red),
                     ),
                   ],
                 ),
-              ],
+              ),
             ),
           ),
         ),
-      ),
     );
   }
 }
