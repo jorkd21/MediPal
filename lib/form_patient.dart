19d9da3eb349ca03d76d8a90a18bee8d031e71ab
kosmckid <kosmckid@csus.edu>
2024-04-03 11:31:08 -0700

    remove debugging prints and patient data page from patient form page

--- lib/form_patient.dart
+++ lib/form_patient.dart
@@ -33,7 +33,7 @@ class _PatientFormState extends State<PatientForm> {
       GeneralInfoForm(patient: _patient, formKey: _generalInfoFormKey),
       HealthConditionsForm(patient: _patient, formKey: _healthConditionsFormKey),
       NextForm(),
-      GetPatientData( patientId: '-NuNPJvREwd61hnesFhZ',),
+      //GetPatientData( patientId: '-NuNPJvREwd61hnesFhZ',),
     ];
   }
 
