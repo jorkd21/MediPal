a7f379decae52235e4b70b7c83af2ea21de365df
Solbjor <s0lbjorandr3w@gmail.com>
2024-05-15 20:44:29 -0700

    Minor changes to patientData

--- lib/main.dart
+++ lib/main.dart
@@ -43,7 +43,7 @@ class MyApp extends StatelessWidget {
       ),
       initialRoute: '/',
       routes: {
-        '/': (context) => HomePage(),
+        '/': (context) => LoginPage(),
         '/Login': (context) => LoginPage(),
         '/SignUp': (context) => SignUpPage(),
         '/PatientForm': (context) => PatientForm(patient: Patient()),
